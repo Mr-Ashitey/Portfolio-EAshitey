96c0f59ddef98413976d158fff4d83173e6ae9fc
Joseph Maclean <josephmacleanarhin@outlook.com>
2021-10-22 15:15:18 +0000

    Added error page

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ app.use(express.static("./public"));
 
 app.use("/", home);
 
-// //404 handler
-// app.use((req, res, next) => {
-//   const error = new Error("Page not found!!!!");
-//   error.status = 404;
-//   return next(error);
-// });
+// 404 handler
+app.use((req, res, next) => {
+  const error = new Error("Page not found!!!!");
+  error.status = 404;
+  return next(error);
+});
 
-// //main error handler
-// app.use((error, req, res, next) => {
-//   return res.status(error.status || 500).render("error", { error });
-// });
+// main error handler
+app.use((error, req, res, next) => {
+  return res.status(error.status || 500).render("error", { error });
+});
 
 //define port on which server will run
 const port = process.env.PORT || 4000;
